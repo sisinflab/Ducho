ae00b99fd1fae990e78e4a404f68eada51cc7025
dmalitesta <d.malitesta@gmail.com>
2023-06-02 15:22:45 +0100

    Demo 1 added. Docker-compose file added.

--- demos/demo1.py
+++ demos/demo1.py
@@ -7,16 +7,16 @@ import os
 
 
 def main(argv):
-    dataset = FashionMNIST(root='../data/demo1/', download=True)
+    dataset = FashionMNIST(root='./data/demo1/', download=True)
     dataset = torch.utils.data.Subset(dataset, np.random.choice(len(dataset), 100, replace=False))
-    if not os.path.exists('../data/demo1/images/'):
-        os.makedirs('../data/demo1/images/')
+    if not os.path.exists('./data/demo1/images/'):
+        os.makedirs('./data/demo1/images/')
     for idx, (img, _) in enumerate(dataset):
-        img.save('../data/demo1/images/{:03d}.jpg'.format(idx))
-    shutil.rmtree('../data/demo1/FashionMNIST/')
+        img.save('./data/demo1/images/{:03d}.jpg'.format(idx))
+    shutil.rmtree('./data/demo1/FashionMNIST/')
     extractor_obj = MultimodalFeatureExtractor(argv=argv)
     extractor_obj.execute_extractions()
 
 
 if __name__ == '__main__':
-    main(['--dataset_path=../data/demo1'])
+    main(['--dataset_path=./data/demo1'])
