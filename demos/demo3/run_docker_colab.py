6972194304348eb4f2abff5db87ecf1e3920fa8b
dmalitesta <d.malitesta@gmail.com>
2023-06-04 23:14:04 +0100

    Demo 3 added (must be tested).

--- demos/demo3/run_docker_colab.py
+++ demos/demo3/run_docker_colab.py
@@ -12,12 +12,12 @@ def main():
     print('Dataset: MUSDB18-HQ - a corpus for music separation')
     print('Visit the original website at: https://sigsep.github.io/datasets/musdb.html#musdb18-compressed-stems')
 
-    songs = os.listdir('./local/data/demo3/songs/train/')
-    random.shuffle(songs)
-    for idx, s in enumerate(songs[:10]):
-        shutil.move(f'./local/data/demo3/songs/train/{s}/mixture.wav', f'./local/data/demo3/songs/{idx}.wav')
-    shutil.rmtree('./local/data/demo3/songs/train/')
-    shutil.rmtree('./local/data/demo3/songs/test/')
+    # songs = os.listdir('./local/data/demo3/songs/train/')
+    # random.shuffle(songs)
+    # for idx, s in enumerate(songs[:10]):
+    #     shutil.move(f'./local/data/demo3/songs/train/{s}/mixture.wav', f'./local/data/demo3/songs/{idx}.wav')
+    # shutil.rmtree('./local/data/demo3/songs/train/')
+    # shutil.rmtree('./local/data/demo3/songs/test/')
     extractor_obj = MultimodalFeatureExtractor(config_file_path='./demos/demo3/demo3.yml')
     extractor_obj.execute_extractions()
 
