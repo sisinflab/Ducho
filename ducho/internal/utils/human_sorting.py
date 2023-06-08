4d404dc9edb926f168c3a7288b3d0bc74609496e
GassiGiuseppe <g.gassi@studenti.poliba.it>
2023-06-08 11:30:37 +0200

    Human sort documentation update

--- ducho/internal/utils/human_sorting.py
+++ ducho/internal/utils/human_sorting.py
@@ -16,8 +16,10 @@ def human_sort(unsorted_list):
     """
     It sorts a list of string both alphabetically and numerically. This means that the order follow the alphabet order
     but also the cardinal one. E.g: ['10','2','1','8'] -> ['1','2','8','10']
-    :param unsorted_list: the list of string to sort
-    :return: the sorted list of string
+    Args:
+        unsorted_list: the list of string to sort
+    Returns:
+        the sorted list of string
     """
     unsorted_list.sort(key=_natural_keys)
     return unsorted_list
