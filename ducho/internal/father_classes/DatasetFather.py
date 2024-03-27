3302b406985f24f4658784e0226a93a56ff2a510
Matteo Attimonelli <matteo.attimonelli1999@gmail.com>
2024-03-27 20:52:23 +0100

    implemented serialization's logic for batch extractions.

--- ducho/internal/father_classes/DatasetFather.py
+++ ducho/internal/father_classes/DatasetFather.py
@@ -73,13 +73,13 @@ class DatasetFather:
     def set_model(self, model):
         self._model_name = model
 
-    def create_output_file(self, index, extracted_data, model_layer, fusion=None):
+    def create_output_file(self, input_batch, extracted_data, model_layer, fusion=None):
         """
         Create an output numpy file with extracted data.
         (E.g. datasetFolder/framework/modelName/modelLayer/fileName.npy)
 
         Args:
-            index (int): The index to the filenames list.
+            input_batch (tensor): The batch just processed by the extractor. It contains the filenames too.
             extracted_data (Any): The data to be stored in the .npy file.
             model_layer (str): The name of the layer.
             fusion (str, optional): The type of fusion for multimodal models.
@@ -89,21 +89,61 @@ class DatasetFather:
 
         """
 
-        # Generate file name
-        input_file_name = self._filenames[index].split('.')[0]
-        output_file_name = input_file_name + '.npy'
-
-        # Generate output path
         backend_library = self._backend_libraries_list[0]
         output_path = os.path.join(self._output_directory_path, backend_library)
         output_path = os.path.join(output_path, os.path.splitext(os.path.basename(self._model_name))[0])
         output_path = os.path.join(output_path, str(model_layer))
+
+        filenames = input_batch[1]
+
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
-        # Create file
-        path = os.path.join(output_path, output_file_name)
-        numpy.save(path, extracted_data)
+        # checking whether batch size is > 1.
+
+        if len(extracted_data) > 1:
+            filenames = [f.split('.')[0] for f in filenames]
+            for f, e in zip(filenames, extracted_data):
+                output_file_name = f + '.npy'
+                path = os.path.join(output_path, output_file_name)
+                numpy.save(path, e)
+
+        else:
+            filenames = filenames.split('.')[0]
+            output_file_name = filenames + '.npy'
+            path = os.path.join(output_path, output_file_name)
+            numpy.save(path, extracted_data)
+    # def create_output_file(self, index, extracted_data, model_layer, fusion=None):
+    #     """
+    #     Create an output numpy file with extracted data.
+    #     (E.g. datasetFolder/framework/modelName/modelLayer/fileName.npy)
+
+    #     Args:
+    #         index (int): The index to the filenames list.
+    #         extracted_data (Any): The data to be stored in the .npy file.
+    #         model_layer (str): The name of the layer.
+    #         fusion (str, optional): The type of fusion for multimodal models.
+
+    #     Returns:
+    #         None
+
+    #     """
+
+    #     # Generate file name
+    #     input_file_name = self._filenames[index].split('.')[0]
+    #     output_file_name = input_file_name + '.npy'
+
+    #     # Generate output path
+    #     backend_library = self._backend_libraries_list[0]
+    #     output_path = os.path.join(self._output_directory_path, backend_library)
+    #     output_path = os.path.join(output_path, os.path.splitext(os.path.basename(self._model_name))[0])
+    #     output_path = os.path.join(output_path, str(model_layer))
+    #     if not os.path.exists(output_path):
+    #         os.makedirs(output_path)
+
+    #     # Create file
+    #     path = os.path.join(output_path, output_file_name)
+    #     numpy.save(path, extracted_data)
 
     @abstractmethod
     def __getitem__(self, idx):
